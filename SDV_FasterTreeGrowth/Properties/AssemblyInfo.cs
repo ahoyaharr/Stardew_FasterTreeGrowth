93608c761b811ffa8b017868489db4c4733e74d2
AhoyAharr <alexanderharrison1994@gmail.com>
2016-11-19 21:55:45 -0800

    Fixed file path

--- SDV_FasterTreeGrowth/Properties/AssemblyInfo.cs
+++ SDV_FasterTreeGrowth/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SDV_KeyListener")]
+[assembly: AssemblyTitle("SDV_FasterTreeGrowth")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SDV_KeyListener")]
+[assembly: AssemblyProduct("SDV_FasterTreeGrowth")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
